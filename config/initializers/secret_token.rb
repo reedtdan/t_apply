9e6001870afcfaa54b6e6abe27b7c7e621af89c4
Madeline Huberth <madelinehuberth@gmail.com>
2012-04-18 19:40:33 -0400

    Second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TApply::Application.config.secret_token = '0d585a3fe6547366ba78fceb19e580c0efec65e0385202761542fe06803361eb990dbdb4d59eaa22271a4c1c69a86f286b7ee17d7c0d88128b8fcedc0d3c69c2'
