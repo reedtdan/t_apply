17eae928b1166713154b93c4b427e18fe78a5ab1
Madeline Huberth <madelinehuberth@gmail.com>
2012-04-18 21:38:11 -0400

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,14 +6,28 @@ describe "Static pages" do
 
     it "should have the content 'Home'" do
       visit '/static_pages/home'
-      page.should have_content('Home')
+      page.should have_selector('h1', :text => 'Home')
     end
-  end
   
-  describe "About page" do
-    it "should have the content 'About'" do
-      visit '/static_pages/about'
-      page.should have_content('About')
+  it "should have the title 'Home'" do
+        visit '/static_pages/home'
+        page.should have_selector('title',
+                          :text => "Telluride House Application | Home")
+      end
     end
-  end
+  
+  
+    describe "About page" do
+
+        it "should have the h1 'About'" do
+          visit '/static_pages/about'
+          page.should have_selector('h1', :text => 'About')
+        end
+
+        it "should have the title 'About Us'" do
+          visit '/static_pages/about'
+          page.should have_selector('title',
+                        :text => "Telluride House Application | About")
+        end
+      end
 end
